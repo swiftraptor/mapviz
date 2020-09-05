d5da8c43adab412c004053392e419c740a5b5896
Swiftraptor <67340509+swiftraptor@users.noreply.github.com>
2020-09-05 11:55:14 +0100

    Initial commit (add ramp json as static asset for caching)

---
+++ src/map/Map.tsx
@@ -0,0 +1,10 @@
+import React, { FunctionComponent } from 'react';
+
+
+export const Map: FunctionComponent<{}> = ({}) => {
+    return (
+        <>
+            <h1>Hello world</h1>
+        </>
+    );
+};
